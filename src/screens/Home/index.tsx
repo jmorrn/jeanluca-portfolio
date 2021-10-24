af93720d868114d18263b7a245f67e026ea31a09
Jeanluca Moreno <Jeanlucam12@gmail.com>
2021-10-24 18:34:19 -0300

    Import of PhotoWrapper to Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,33 +1,13 @@
 import React from 'react'
 
-import Button from '@components/Button'
-import Footer from '@components/Footer'
-import Menu from '@components/Menu'
+import { PhotoWrapper } from '@components/PhotoWrapper'
 
 import { Container } from './styles'
 
 const Home: React.FC = () => {
   return (
     <Container>
-      <section className="container">
-        <Menu />
-        <div className="content">
-          <h1>
-            Olá, me chamo{' '}
-            <span className="blue-gradient"> Jeanluca Moreno</span>!
-          </h1>
-          <p className="blue-gradient" id="vision">
-            Visão geral
-          </p>
-          <p>hello world</p>
-          <Button
-            title="Currículo"
-            color="var(--white)"
-            icon="FaFileDownload"
-          />
-        </div>
-      </section>
-      <Footer />
+      <PhotoWrapper />
     </Container>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,46 +3,9 @@ import styled from 'styled-components'
 export const Container = styled.main`
   position: relative;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   min-height: 100vh;
   overflow: hidden;
-
-  background: var(--black);
-
-  .container {
-    width: 100%;
-    margin: 0px auto;
-    height: calc(100vh - 100px);
-    max-width: 1440px;
-  }
-
-  .content {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    color: var(--white);
-    max-width: 1064px;
-    padding: 1rem 3rem;
-    margin: 0 auto;
-    h1 {
-      font-size: 3rem;
-    }
-
-    #vision {
-      margin-bottom: 1rem;
-      font-weight: 600;
-    }
-    .blue-gradient {
-      background: linear-gradient(
-        180deg,
-        var(--blue-100) 0%,
-        var(--blue-500) 97.62%
-      );
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;
-      letter-spacing: 0.09em;
-    }
-  }
 `
