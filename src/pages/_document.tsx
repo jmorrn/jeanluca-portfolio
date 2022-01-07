ec829750a8133493a0918f53266b77c02e9866b3
Jeanluca Moreno <Jeanlucam12@gmail.com>
2022-01-06 22:57:35 -0300

    Changing Fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -58,12 +58,17 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
 
-          {/* Montsserat */}
+          {/* Philosopher */}
           <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Philosopher:wght@400;700&display=swap"
             rel="stylesheet"
           />
         </Head>
+        {/* Mulish */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Mulish&display=swap"
+          rel="stylesheet"
+        />
         <body>
           <Main />
           <NextScript />
