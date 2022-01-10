34f5bf9306cf24eda71a59f3f110d6c0f595de0a
Jeanluca-Moren0 <jeanlucam12@gmail.com>
2022-01-09 22:03:15 -0300

    Modified Components structure and added label

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,6 +4,8 @@ import { api } from 'src/services/api'
 
 import Layout from '@components/Layout'
 
+import { Paragraph } from './styles'
+
 const Home: React.FC = () => {
   useEffect(() => {
     api.get('blog').then(response => {
@@ -11,20 +13,20 @@ const Home: React.FC = () => {
     })
   }, [])
   return (
-    <Layout>
-      <p>
+    <Layout label="Olá, me chamo Jeanluca Moreno!">
+      <Paragraph>
         Seja bem-vindo(a) ao meu portfólio!! Tenho 19 anos. Esse é o meu segundo
         modelo de portfólio, e o intuito dele é ser um projeto que eu vou me
         empenhando e refatorando com o tempo, ou seja,mais conhecimentos igual
         mais features e menos bugs.
-      </p>
+      </Paragraph>
 
-      <p>
+      <Paragraph>
         Atualmente trabalho como Estágiario de TI no Hospital Sírio Libânes e
         também sou desenvolvedor e um dos fundadores da Monts.
-      </p>
+      </Paragraph>
 
-      <p>
+      <Paragraph>
         Falando um pouco mais do meu começo e objetivos na área de TI,iniciei os
         estudos em 2017, quando queria ainda ser um desenvolvedor de games,
         iniciei estudando HTML, CSS e Javascript. Em 2018, fiz curso técnico na
@@ -32,20 +34,12 @@ const Home: React.FC = () => {
         conheço hoje, Banco de dados, Front-End, Back-End, Modelagem de dados,
         Estudos de casos, fluxograma, e muito mais! Em 2020, iniciei a faculdade
         em Informática para Negócios, e irei finaliza-lá em 2023.
-      </p>
+      </Paragraph>
 
-      <p>
+      <Paragraph>
         Essa página de início é só um overview das coisas. Espero que gostem do
         conteúdo que vou colocando aqui.
-      </p>
-
-      <a
-        href="./JeanlucaMorenoC.pdf"
-        className="buttonCurriculum"
-        target="_blank"
-      >
-        Currículo
-      </a>
+      </Paragraph>
     </Layout>
   )
 }
