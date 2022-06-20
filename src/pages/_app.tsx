a0f3c4050c303b9c055006bb1b51224cd16cc417
Jeanluca-Moren0 <jeanlucam12@gmail.com>
2022-06-20 00:13:06 -0300

    fix: removed styled global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,12 @@ import React from 'react'
 
 import { AppProps } from 'next/app'
 
-import GlobalStyles from '../styles/global'
+import '../styles/global.css'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Component {...pageProps} />
-      <GlobalStyles />
     </>
   )
 }
